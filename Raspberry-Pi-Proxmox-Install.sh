56af892da50905163a50ed4f708ef6bc636c9b76
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-04 14:54:36 +1100

    Update Raspberry-Pi-Proxmox-Install.sh

--- Raspberry-Pi-Proxmox-Install.sh
+++ Raspberry-Pi-Proxmox-Install.sh
@@ -3,17 +3,6 @@
 echo "LSA SERVER STARTUP SCRIPT - RASPBERRY PI PROXMOX INSTALL"
 echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
 
-# ip=$(hostname -I)
-# ip=$(echo "$ip" | sed '/^/ s/ .*//')
-
-# curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh > RPiOS64-IA-Install.sh
-# chmod +x RPiOS64-IA-Install.sh
-# ./RPiOS64-IA-Install.sh
-
-
-# echo "PROXMOX IS RUNNING ON http://$ip:8006"
-# echo "LSA SERVER STARTUP SCRIPT COMPLETED."
-
 echo "INSTALLATION CAN'T BE DONE DIRECTLY FROM THIS SCRIPT. PLEASE RUN THE FOLLOWING COMMANDS WHEN THE SCRIPT EXITS"
 echo "curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh > RPiOS64-IA-Install.sh"
 echo "chmod +x RPiOS64-IA-Install.sh"
