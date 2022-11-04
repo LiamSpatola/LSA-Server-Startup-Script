58564f48fb64228ac25699acb385cf14c8840cc0
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-04 14:50:40 +1100

    Update Raspberry-Pi-Proxmox-Install.sh

--- Raspberry-Pi-Proxmox-Install.sh
+++ Raspberry-Pi-Proxmox-Install.sh
@@ -1,7 +1,7 @@
-# #!/bin/bash
+#!/bin/bash
 
-# echo "LSA SERVER STARTUP SCRIPT - RASPBERRY PI PROXMOX INSTALL"
-# echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
+echo "LSA SERVER STARTUP SCRIPT - RASPBERRY PI PROXMOX INSTALL"
+echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
 
 # ip=$(hostname -I)
 # ip=$(echo "$ip" | sed '/^/ s/ .*//')
@@ -14,4 +14,19 @@
 # echo "PROXMOX IS RUNNING ON http://$ip:8006"
 # echo "LSA SERVER STARTUP SCRIPT COMPLETED."
 
-curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh | bash
+echo "INSTALLATION CAN'T BE DONE DIRECTLY FROM THIS SCRIPT. PLEASE RUN THE FOLLOWING COMMANDS WHEN THE SCRIPT EXITS:
+echo "curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh > RPiOS64-IA-Install.sh"
+echo "chmod +x RPiOS64-IA-Install.sh"
+echo "./RPiOS64-IA-Install.sh"
+echo "WE APOLOGISE FOR THE INCONVENIENCE."
+
+echo "EXITING IN 3 SECONDS."
+timer_start=$SECONDS
+full_time=(( timer_start + 3 ))
+
+until [ $SECONDS == $full_time];
+do
+  
+done
+
+echo "LSA SERVER STARTUP SCRIPT COMPLETED."
