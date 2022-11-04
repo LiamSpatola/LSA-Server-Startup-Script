f02c0287f652e1f2e6a343ca93ccd060adf25260
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-04 14:39:13 +1100

    Update Raspberry-Pi-Proxmox-Install.sh

--- Raspberry-Pi-Proxmox-Install.sh
+++ Raspberry-Pi-Proxmox-Install.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "LSA SERVER STARTUP SCRIPT - RASPBERRY PI PROXMOX INSTALL"
-echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
+# echo "LSA SERVER STARTUP SCRIPT - RASPBERRY PI PROXMOX INSTALL"
+# echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
 
-ip=$(hostname -I)
-ip=$(echo "$ip" | sed '/^/ s/ .*//')
+# ip=$(hostname -I)
+# ip=$(echo "$ip" | sed '/^/ s/ .*//')
 
-curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh > RPiOS64-IA-Install.sh
-chmod +x RPiOS64-IA-Install.sh
-./RPiOS64-IA-Install.sh
+# curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh > RPiOS64-IA-Install.sh
+# chmod +x RPiOS64-IA-Install.sh
+# ./RPiOS64-IA-Install.sh
 
 
-echo "PROXMOX IS RUNNING ON http://$ip:8006"
-echo "LSA SERVER STARTUP SCRIPT COMPLETED."
+# echo "PROXMOX IS RUNNING ON http://$ip:8006"
+# echo "LSA SERVER STARTUP SCRIPT COMPLETED."
+
+curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh | bash
