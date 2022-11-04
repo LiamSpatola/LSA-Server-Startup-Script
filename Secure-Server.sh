513ed3a5bc9086e1a3282da216d2ef02d7075541
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-04 15:48:18 +1100

    Update Secure-Server.sh

--- Secure-Server.sh
+++ Secure-Server.sh
@@ -1,1 +1,19 @@
+echo "LSA SERVER STARTUP SCRIPT - SECURE SERVER"
+echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
+echo ""
 
+apt update
+apt install unattended-upgrades
+dpkg-reconfigure --priority=low unattended-upgrades
+apt install ufw
+ufw enable
+ufw reload
+
+echo ""
+echo "====================================================================================="
+echo "INSTALLATION COMPLETE. TO ALLOW A PORT THROUGH THE FIREWALL USE THE FOLLOWING COMMAND"
+echo "ufw allow {port number}"
+echo "====================================================================================="
+
+echo ""
+echo "LSA SERVER STARTUP SCRIPT COMPLETED."
