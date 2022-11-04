1f7708940676cf44ea86641ccdc5865b937623c7
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-04 15:04:24 +1100

    Update Pi-Hole-Install.sh

--- Pi-Hole-Install.sh
+++ Pi-Hole-Install.sh
@@ -2,6 +2,7 @@
 
 echo "LSA SERVER STARTUP SCRIPT - PI-HOLE INSTALL"
 echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
+echo ""
 
 ip=$(hostname -I)
 ip=$(echo "$ip" | sed '/^/ s/ .*//')
@@ -10,5 +11,6 @@ apt-get update -y
 curl -sSL https://install.pi-hole.net | bash
 pihole -a -p
 
+echo ""
 echo "PI-HOLE GUI IS RUNNING ON http://$ip"
 echo "LSA SERVER STARTUP SCRIPT COMPLETED."
