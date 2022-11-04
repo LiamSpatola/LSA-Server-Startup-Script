af7bdad788b64534a22f45f0033934cb2100c4bd
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-05 10:13:34 +1100

    Update Docker-Install.sh

--- Docker-Install.sh
+++ Docker-Install.sh
@@ -13,5 +13,9 @@ docker volume create portainer_stuff
 docker run -d -p 9443:9443 -p 8000:8000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_stuff:/data portainer/portainer-ce:latest
 
 echo ""
+echo "============================================================"
 echo "DOCKER GUI PORTAL (PORTAINER) IS RUNNING ON https://$ip:9443"
+echo "============================================================"
+echo ""
+
 echo "LSA SERVER STARTUP SCRIPT COMPLETED."
