f47db9a6a7c45df7965b32410a20f9175afd375c
LiamSpatola <109417820+LiamSpatola@users.noreply.github.com>
2022-11-05 17:31:47 +1100

    Update OpenMediaVault-Install.sh

--- OpenMediaVault-Install.sh
+++ OpenMediaVault-Install.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+echo "LSA SERVER STARTUP SCRIPT - OPENMEDIAVAULT INSTALL"
+echo "RUN THIS SCRIPT AS ROOT. DO NOT USE SUDO."
+echo ""
+
+ip=$(hostname -I)
+ip=$(echo "$ip" | sed '/^/ s/ .*//')
+
+apt update && apt upgrade
+
+wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | bash
+
+echo ""
+echo "================================================================="
+echo "INSTALLATION SUCCESFUL. GUI IS RUNNING ON http://$ip"
+echo "================================================================="
+echo ""
+echo "LSA SERVER STARTUP SCRIPT COMPLETED."
